3c5ace6c5b87979d2cf79ffa8023835149ec2cde
Michael Lorang <mlorang@udel.edu>
2023-02-20 13:10:03 -0500

    solved functions 3 through 5

--- src/functions.ts
+++ src/functions.ts
@@ -30,7 +30,7 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    return message.toUpperCase() + "!";
 }
 
 /**
@@ -38,7 +38,8 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    const len: number = message.length;
+    return message.charAt(len - 1) === "?";
 }
 
 /**
@@ -47,5 +48,11 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    if (word.toLowerCase() === "yes") {
+        return true;
+    } else if (word.toLowerCase() === "no") {
+        return false;
+    } else {
+        return null;
+    }
 }
