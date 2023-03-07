e609d2f80158304cd5e0b679b246dc8c789c3450
Michael Lorang <mlorang@udel.edu>
2023-03-07 00:02:54 -0500

    Done work for the day. 3/7/23

--- src/nested.ts
+++ src/nested.ts
@@ -101,7 +101,22 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    const questionCSV = questions.map((question : Question): string => ${question.name},$)
+    const str = "id,name,options,points,published\n";
+    const questionCSV = questions
+        .map(
+            (question: Question): string =>
+                question.id +
+                "," +
+                question.name +
+                "," +
+                question.options.length +
+                "," +
+                question.points +
+                "," +
+                question.published
+        )
+        .join("\n");
+    return str + questionCSV;
 }
 
 /**
@@ -110,7 +125,10 @@ export function toCSV(questions: Question[]): string {
  * making the `text` an empty string, and using false for both `submitted` and `correct`.
  */
 export function makeAnswers(questions: Question[]): Answer[] {
-    return [];
+    let answer: Answer;
+    const answers = questions.map(
+        (question: Question): Answer => (answer.questionId = question.id)
+    );
 }
 
 /***
