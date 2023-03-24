fef424ccfb8b818b29941822e9b0a387411c77cd
Michael Lorang <mlorang@udel.edu>
2023-03-23 20:18:41 -0400

    GiveAttempts comp leted

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -22,13 +22,3 @@ export function CheckAnswer({
         </div>
     );
 }
-
-/*  
-The Check Answer component allows a user to enter an answer to a short answer question, 
-and then displays whether they got it correct or incorrect. 
-The component takes in a parameter representing the expectedAnswer.
-
- * You will need a state to handle the user’s given answer
- * The user’s given answer should initially be the empty string
- * When the user’s given answer matches the expectedAnswer, display ✔️; otherwise display ❌
-*/
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,22 +1,37 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [request, setRequest] = useState<string>("0");
+    const [temp, setTemp] = useState<number>(3);
+
     return (
         <div>
             <h3>Give Attempts</h3>
+            <div>
+                Attempts: {temp} / Requested Attempts: {request}
+            </div>
+            <div>
+                <Button onClick={() => setTemp(temp - 1)} disabled={temp <= 0}>
+                    Use
+                </Button>
+                <Button
+                    onClick={() => setTemp(parseInt(request) + temp)}
+                    disabled={request === ""}
+                >
+                    Gain
+                </Button>
+            </div>
+            <Form.Group controlId="formAttemptRequest">
+                <Form.Label>Enter Requested Attempts</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={request}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setRequest(event.target.value)
+                    }
+                />
+            </Form.Group>
         </div>
     );
 }
-/*
-GiveAttempts
-The GiveAttempts component simulates a user taking a quiz with a limited number of attempts, 
-but also provides a way for them to gain as many attempts as they want via a numeric input box.
-
- * You will need a state to represent the number of attempts the user has left, and another state to represent the number of attempts they are requesting.
- * The initial number of attempts left should be 3.
- * The number of attempts left should be visible.
- * There should be a numeric input box where the user can specify their requested number of attempts.
- * There should be two buttons, one labeled use that decreases the attempts by one and one labeled gain that increases the attempts by the amount in the input box.
- * If the user attempts to request an invalid amount (e.g., the empty string "") that cannot be parsed as an integer, then do not change their number of attempts.
- * When the user is out of attempts, the use button should be disabled
-*/
