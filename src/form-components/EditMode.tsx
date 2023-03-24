41f44222fdb463aef36fd3c5b551ae6bdedd7ab1
Michael Lorang <mlorang@udel.edu>
2023-03-24 14:53:02 -0400

    All components functional. Failing some test cases. Will fix later

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,56 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    const [editMode, setEditMode] = useState<boolean>(false);
+    const [studentName, setStudentName] = useState<string>("Your Name");
+    const [isStudent, setIsStudent] = useState<boolean>(true);
+
+    function updateEditMode(event: React.ChangeEvent<HTMLInputElement>) {
+        setEditMode(event.target.checked);
+    }
+
+    function updateStudentName(event: React.ChangeEvent<HTMLInputElement>) {
+        setStudentName(event.target.value);
+    }
+
+    function updateIsStudent(event: React.ChangeEvent<HTMLInputElement>) {
+        setIsStudent(event.target.checked);
+    }
+
     return (
         <div>
             <h3>Edit Mode</h3>
+            <Form.Check
+                inline
+                type="switch"
+                id="is-edit-mode-check"
+                label="Set Edit Mode"
+                checked={editMode}
+                onChange={updateEditMode}
+            />
+            <Form.Check
+                inline
+                type="checkbox"
+                id="student-check"
+                label="Are you a student"
+                name="Are you a student"
+                checked={isStudent}
+                onChange={updateIsStudent}
+            />
+
+            <Form.Group controlId="formStudentName">
+                <Form.Label>Input Name</Form.Label>
+                <Form.Control
+                    disabled={editMode === false}
+                    value={studentName}
+                    onChange={updateStudentName}
+                />
+            </Form.Group>
+
+            {isStudent === true
+                ? studentName + " is a student"
+                : studentName + " is not a student"}
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,25 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>(options[0]);
+
+    function updateAnswer(event: React.ChangeEvent<HTMLSelectElement>) {
+        setAnswer(event.target.value);
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="optionChoices">
+                <Form.Label>Options / Choice: {answer}</Form.Label>
+                <Form.Select value={answer} onChange={updateAnswer}>
+                    <Form.Check inline></Form.Check>{" "}
+                    {options.map((option: string) => (
+                        <option key={option} value={option}></option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {answer === expectedAnswer ? <span>✔️</span> : <span>❌</span>}
         </div>
     );
 }
